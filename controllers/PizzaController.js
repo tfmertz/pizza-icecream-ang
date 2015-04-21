a71f518628f9c2d418377462015e24a1102a74e6
tanner heffner & Tom Mertz <student@epicodus.com>
2015-04-21 14:31:30 -0700

    Add deletePizza function

--- controllers/PizzaController.js
+++ controllers/PizzaController.js
@@ -1,13 +1,15 @@
 Restaurant.controller('PizzaCtrl', function PizzaCtrl($scope, RestaurantFactory) {
   $scope.pizzas = [];
   $scope.RestaurantFactory = RestaurantFactory;
+  var pizzaCounter = 0;
 
   $scope.addPizza = function() {
 
     var pizzaPrice = {'slice': 2, 'small': 10, 'medium': 12, 'large': 14};
 
-    $scope.pizzas.push({size: $scope.pizzaSize, price: pizzaPrice[$scope.pizzaSize] })
+    $scope.pizzas.push({id: pizzaCounter, size: $scope.pizzaSize, price: pizzaPrice[$scope.pizzaSize] })
     $scope.pizzaSize = null;
+    pizzaCounter++;
   };
 
   $scope.pizzaPrice = function() {
@@ -17,7 +19,8 @@ Restaurant.controller('PizzaCtrl', function PizzaCtrl($scope, RestaurantFactory)
   };
 
   $scope.deletePizza = function(pizza) {
-    
+    var index = $scope.pizzas.indexOf(pizza);
+    $scope.pizzas.splice(index, 1);
   };
 
 });
